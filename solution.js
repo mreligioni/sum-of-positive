ec511d606cd132da59748e3f2a248404a91f2b54
Mateusz <87975831+mreligioni@users.noreply.github.com>
2021-10-25 21:14:21 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 function positiveSum(arr) {
-    var sum = 0;
-    for(var i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
       if(arr[i] > 0) {
        sum += arr[i];
      }
